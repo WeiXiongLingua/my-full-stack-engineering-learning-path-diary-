6a1e724c6e1aca0caa5e5f3ad1d402c1c887e781
TopHacker <seraphzip1@gmail.com>
2025-12-20 15:29:43 +0800

    ok

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ tds.forEach(td => {
     td.onclick = function () {
         td.style.backgroundColor = '#222';
     }
+    td.onmouseleave = function () {
+        td.style.backgroundColor = 'red';
+    }
 })
