cadb1acd924ca20ca5f73a294fcc1d8a0167da90
TopHacker <seraphzip1@gmail.com>
2025-12-20 16:26:07 +0800

    today's lesson

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 I’m trying to use only English and German so I can improve faster.
 
-What is System Design? 
-
-I am a man.
-This video want to explain how system design work.
-
-I very like this video and it have much information.
+OK, I ended up at episode 59 today. Big Improvement.
--- js.js
+++ js.js
@@ -3,28 +3,18 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        const html = fs.readFileSync(path.join(__dirname, 'index.html'),
-            'utf-8'
-        )
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end(html);
-    } else if (req.url === '/style.css') {
-        const css = fs.readFileSync(
-            path.join(__dirname, 'style.css'),
-            'utf-8'
-        )
-        res.setHeader('Content-Type', 'text/css')
-        res.end(css)
-    } else if (req.url === '/script.js') {
-        const js = fs.readFileSync(
-            path.join(__dirname, 'script.js'),
-            'utf-8'
-        )
-        res.setHeader('Content-Type', 'text/javascript')
-        res.end(js)
-    }
+    let { pathname } = new URL(req.url, 'http://127.0.0.1');
+    let filePath = __dirname + '/page' + pathname;
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end('Failed');
+            return;
+        }
+        res.end(data);
+    })
 
 })
 
 server.listen(9000)
+
