b239f784f44e9d32c77a93bd068c9debfb20e133
TopHacker <seraphzip1@gmail.com>
2025-12-22 20:12:45 +0800

    103

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Come on.
 Right Now, I am at episode 73. 200 is the end. I am already half way there. Come on.
 
 Right Now, I am at episode 85. 200 is the end. Come on!
+
+
+Right Now, I am at episode 102. Half way there. Sehr gut.
+
+Right Now, I am at episode 103. I will land a job in the future.
+
+
--- express1/learn.js
+++ express1/learn.js
@@ -1,12 +1,46 @@
 const express = require('express');
-
+const data = require('./learn.json');
 const app = express();
+const path = require('path');
+const { users } = data;
 
+app.get('/other', (req, res) => {
+    res.redirect('https://www.baidu.com');
 
-app.get('/:id.html', (req, res) => {
-    res.send('Content File' + req.params.id);
-    console.log(req.params.id)
 })
+
+app.get('/js', (req, res) => {
+    const file = path.join(__dirname, 'source.txt');
+    res.download(file, 'hahha');
+})
+// app.get('/:age.html', (req, res) => {
+
+//     let { age } = req.params;
+//     const user = users.find(u => u.age === Number(age))
+//     if (!user) {
+//         return res.status(404).send('User not found');
+//     }
+
+//     res.end(`
+//         <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <form action="http://127.0.0.1:3000/login" method="post">
+//         <button>submit</button>
+//     </form>
+//     <h3>${user.name}</h3>
+//     <h3>${user.age}</h3>
+// </body>
+// </html>
+//         `)
+// })
+
 app.listen(3000, () => {
     console.log('running');
 })
+
