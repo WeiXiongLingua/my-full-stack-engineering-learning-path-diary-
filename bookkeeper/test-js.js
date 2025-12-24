f6cd7b80948c6d96953cb15576e8ee1ec8d68abd
TopHacker <seraphzip1@gmail.com>
2025-12-24 14:45:41 +0800

    google

--- bookkeeper/test-js.js
+++ bookkeeper/test-js.js
@@ -1,17 +1,8 @@
-const name = "jack"
-const age = 20
-const msg = `My name is ${name}, I am ${age} old`
-
-function result() {
-    return 1
-}
-
-const answer = `Result: ${result()}`
-console.log(answer)
-
-function now() {
-    return new Date().toISOString()
+export function add(a, b) {
+    return a + b
   }
   
-  console.log(`[${now()}] Server started`)
+  export function sub(a, b) {
+    return a - b
+  }
   
---
+++ esm-demo/main.js
@@ -0,0 +1,8 @@
+import xxx, { add, mul, sub } from './math.js'
+
+console.log(add(2, 3)) // 5
+console.log(mul(2, 3)) // 6
+console.log(sub(5, 2))
+
+
+xxx()
